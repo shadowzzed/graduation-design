9c05e59127501ea457d3b09da8d94a5e27166fcd
Zed <shadowl91@163.com>
2019-12-21 16:06:24 +0800

    finish multiple threads get msg

--- src/main/java/com/zed/design/reptile/WebReptileCNKI.java
+++ src/main/java/com/zed/design/reptile/WebReptileCNKI.java
@@ -8,10 +8,9 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import com.zed.design.core.Config;
 import com.zed.design.core.pojo.Paper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.internal.StringUtil;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -22,9 +21,9 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,6 +33,7 @@ import java.util.regex.Pattern;
  * @contact shadowl91@163.com
  */
 @Component
+@Slf4j
 public class WebReptileCNKI {
 
     @Autowired
@@ -66,25 +66,9 @@ public class WebReptileCNKI {
     public Document getCNKIContent(String keyword) {
         List<String> nextPageList = new ArrayList<>();
         WebClient webClient = this.getWebClient();
-        // get html page
-        HtmlPage htmlPage = null;
         Document document = null;
         try {
-            htmlPage = webClient.getPage(config.cnkiURL);
-            if (htmlPage == null)
-                throw new NullPointerException("get no page");
-            HtmlTextInput input = (HtmlTextInput) htmlPage.getElementById("txt_1_value1");
-            input.setValueAttribute(keyword);
-            DomElement btnSearch = htmlPage.getElementById("btnSearch");
-            HtmlPage htmlPage1 = (HtmlPage) btnSearch.click();
-            String url = "https://kns.cnki.net/kns/brief/brief.aspx?pagename=ASP.brief_result_aspx&isinEn=1&dbPrefix=SCDB&dbCatalog=%e4%b8%ad%e5%9b%bd%e5%ad%a6%e6%9c%af%e6%96%87%e7%8c%ae%e7%bd%91%e7%bb%9c%e5%87%ba%e7%89%88%e6%80%bb%e5%ba%93&ConfigFile=SCDB.xml&research=off&t=";
-            url += System.currentTimeMillis();
-            url += "&keyValue=";
-            url += URLEncoder.encode(keyword, "utf-8");
-            url += "&S=1&sorttype=";
-            HtmlPage targetPage = (HtmlPage) webClient.getPage(url);
-            // get content
-            document = Jsoup.parse(targetPage.asXml());
+            document = this.getContentPapers(keyword, webClient);
             // get table
             Elements content = document.getElementsByClass("pageBar_top").select("tr");
             // get each row
@@ -93,10 +77,22 @@ public class WebReptileCNKI {
             // get all page urls
             String total_temp = content.get(0).select("span.countPageMark").text();
             int totalPages = Integer.parseInt(total_temp.substring(2));
-            this.addURLs(nextPageList, totalPages);
-            for (String nextURL: nextPageList)
-                this.getNextPagePaper(nextURL, webClient);
-        } catch (IOException e) {
+            log.info("keyword:{},totalPages:{}",keyword, totalPages);
+//            this.addURLs(nextPageList, totalPages);
+//            int count = 1;
+//            for (String nextURL: nextPageList) {
+//                log.info("this page is:{}", ++count);
+//                this.getNextPagePaper(nextURL, webClient);
+//            }
+            int num = totalPages / 20;
+            ExecutorService executorService = Executors.newFixedThreadPool(num + 1);
+            int start = 0;
+            while (start < num) {
+                executorService.execute(new CNKIRunnable(start, keyword));
+                start++;
+            }
+            Thread.sleep(100000L);
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         } finally {
             webClient.close();
@@ -104,14 +100,43 @@ public class WebReptileCNKI {
         return document;
     }
 
+    private Document getContentPapers(String keyword, WebClient webClient) throws IOException {
+        this.searchAndClickBtn(webClient, keyword);
+        String url = "https://kns.cnki.net/kns/brief/brief.aspx?pagename=ASP.brief_result_aspx&isinEn=1&dbPrefix=SCDB&dbCatalog=%e4%b8%ad%e5%9b%bd%e5%ad%a6%e6%9c%af%e6%96%87%e7%8c%ae%e7%bd%91%e7%bb%9c%e5%87%ba%e7%89%88%e6%80%bb%e5%ba%93&ConfigFile=SCDB.xml&research=off&t=";
+        url += System.currentTimeMillis();
+        url += "&keyValue=";
+        url += URLEncoder.encode(keyword, "utf-8");
+        url += "&S=1&sorttype=";
+        log.info("url:{}", url);
+        HtmlPage targetPage = (HtmlPage) webClient.getPage(url);
+        return Jsoup.parse(targetPage.asXml());
+    }
+
+    // do click
+    private void searchAndClickBtn(WebClient webClient, String keyword) throws IOException {
+        HtmlPage htmlPage = webClient.getPage(config.cnkiURL);
+        if (htmlPage == null)
+            throw new NullPointerException("get no page");
+        HtmlTextInput input = (HtmlTextInput) htmlPage.getElementById("txt_1_value1");
+        input.setValueAttribute(keyword);
+        DomElement btnSearch = htmlPage.getElementById("btnSearch");
+        btnSearch.click();
+    }
+
     // add urls to list
     private void addURLs(List<String> urls, int total) {
-        StringBuilder url = new StringBuilder("https://kns.cnki.net/kns/brief/brief.aspx?curpage=");
         for (int i = 2; i < total; i++) {
-            url.append(i);
-            url.append("&RecordsPerPage=20&QueryID=3&ID=&turnpage=1&tpagemode=L&dbPrefix=SCDB&Fields=&DisplayMode=listmode&PageName=ASP.brief_result_aspx&isinEn=1&");
-            urls.add(url.toString());
+            String url = "https://kns.cnki.net/kns/brief/brief.aspx?curpage=" + i +
+                    "&RecordsPerPage=20&QueryID=3&ID=&turnpage=1&tpagemode=L&dbPrefix=SCDB&Fields=&DisplayMode=listmode&PageName=ASP.brief_result_aspx&isinEn=1&";
+            urls.add(url);
         }
+    }
+
+    private String getPageUrl(int i) {
+
+        return "https://kns.cnki.net/kns/brief/brief.aspx?curpage=" + i +
+                "&RecordsPerPage=20&QueryID=3&ID=&turnpage=1&tpagemode=L&dbPrefix=SCDB&Fields=&DisplayMode=listmode&PageName=ASP.brief_result_aspx&isinEn=1&";
+
     }
 
     // get papers in next page
@@ -119,8 +144,45 @@ public class WebReptileCNKI {
         HtmlPage page = (HtmlPage) webClient.getPage(url);
         Document document = Jsoup.parse(page.asXml());
         Elements rows = document.getElementsByClass("pageBar_top").select("tr");
+        log.info("document:{}", document);
         for (Element row: rows)
             this.getPaper(row);
+    }
+
+    private void getNextPagePaperRunnable() {
+
+    }
+
+    class CNKIRunnable implements Runnable {
+
+        private int startPosition;
+
+        private int offset = 20;
+
+        private String keyword;
+
+        public CNKIRunnable(int startPosition, String keyword) {
+            this.startPosition = startPosition;
+            this.keyword = keyword;
+        }
+
+        @Override
+        public void run() {
+            WebClient webClient = getWebClient();
+            try {
+                Document document = getContentPapers(keyword, webClient);
+                int count = 0;
+                while (count < offset) {
+                    count++;
+                    String url = getPageUrl(count + startPosition);
+                    getNextPagePaper(url, webClient);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                webClient.close();
+            }
+        }
     }
 
     private WebClient getWebClient(){
@@ -129,7 +191,7 @@ public class WebReptileCNKI {
         webClient.getOptions().setCssEnabled(false);
         webClient.getOptions().setJavaScriptEnabled(true);
         webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-        webClient.waitForBackgroundJavaScript(3000);
+        webClient.waitForBackgroundJavaScript(30000);
         return webClient;
     }
 
@@ -143,7 +205,7 @@ public class WebReptileCNKI {
                 case 1:
                     paper.setName(td.get(i).text());
                     String href = td.get(i).getElementsByTag("a").attr("href");
-                    paper = this.getSummary(href);
+                    this.getSummary(href, paper);
                     break;
                 case 2:
                     paper.setAuthor(td.get(i).text());
@@ -167,12 +229,12 @@ public class WebReptileCNKI {
                     break;
             }
         }
+        log.info("{}",paper);
         // TODO save into db
         return paper;
     }
 
-    private Paper getSummary(String href) throws IOException {
-        Paper paper = new Paper();
+    private Paper getSummary(String href, Paper paper) throws IOException {
         String url = "https://kns.cnki.net/KCMS/detail/detail.aspx?";
         Matcher dbcode = dbcode_pattern.matcher(href);
         Matcher dbname = dbname_pattern.matcher(href);
@@ -193,11 +255,19 @@ public class WebReptileCNKI {
             url = url + "&filename=" + filename_str;
             paper.setFilecode(filename_str);
         }
-        Document document = Jsoup.connect(url)
-                .userAgent(UA)
-                .get();
+        log.info("detail url:{}",url);
+        Document document = null;
+        try {
+            document = Jsoup.connect(url)
+                    .userAgent(UA)
+                    .timeout(10000)
+                    .get();
+        } catch (IOException e) {
+//            e.printStackTrace();
+            log.error("error url:{}", url);
+        }
         // TODO if is null there seem to be post on other websites still need to process
-        if (document.getElementById("ChDivSummary") == null)
+        if (document == null || document.getElementById("ChDivSummary") == null)
             paper.setSummary(null);
         else
             paper.setSummary(document.getElementById("ChDivSummary").text());
